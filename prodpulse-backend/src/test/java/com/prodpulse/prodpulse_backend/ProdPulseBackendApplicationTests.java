5afb7c2aa14df4bbe60d62ec207e108f7f191c90
Sumeet-Y1 <sumeety202@gmail.com>
2025-12-29 22:38:34 +0530

    feat: initial Spring Boot backend setup with Google GenAI

---
+++ prodpulse-backend/src/test/java/com/prodpulse/prodpulse_backend/ProdPulseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prodpulse.prodpulse_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdPulseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
